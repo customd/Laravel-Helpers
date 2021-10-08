edc0b5eab6c5b0ed1a14f8e554664482650d548e
Craig Smith <craig.smith@customd.com>
2021-10-08 16:36:49 +1300

    docs: added deprecation notice for using pure laravel for this method later

--- src/Listeners/NotificationSendingListener.php
+++ src/Listeners/NotificationSendingListener.php
@@ -3,12 +3,14 @@
 namespace CustomD\LaravelHelpers\Listeners;
 
 use Illuminate\Notifications\Events\NotificationSending;
+use Illuminate\Support\Facades\Log;
 
 class NotificationSendingListener
 {
     public function handle(NotificationSending $event)
     {
         if (method_exists($event->notification, 'blockSending')) {
+            Log::debug("this listener is Deprected and will be removed with Laravel 9 release, please see https://laravel-news.com/laravel-8-50-0");
             return ! $event->notification->blockSending($event->notifiable);
         }
 
