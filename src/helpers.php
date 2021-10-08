a786a6874d9be25b57621360d007f2ee85eade77
Craig Smith <craig.smith@customd.com>
2021-10-08 16:33:17 +1300

    feat: execute helper no longer manipulates data
    BREAKING CHANGE - arrays are no longer flattened

--- src/helpers.php
+++ src/helpers.php
@@ -3,7 +3,6 @@
 if (! function_exists('execute')) {
     function execute(string $classname, ...$args)
     {
-        $args = collect($args)->flatten(1);
         return resolve($classname)->execute(...$args);
     }
 }
