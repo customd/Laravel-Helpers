4a6e0f45f65d59d8f0c5daf7b4657c8191f759a5
Craig Smith <craig.smith@customd.com>
2022-04-07 15:43:14 +1200

    feat: set config based on users current timezone

---
+++ src/Http/Middleware/UserTimeZone.php
@@ -0,0 +1,43 @@
+
+<?php
+namespace CustomD\LaravelHelpers\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+
+class UserTimeZone {
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $this->setTimeZone($request);
+        return $next($request);
+    }
+
+    /**
+     * sets the time zone from request or from the user setting
+     * @param Illuminate\Http\Request $request
+     */
+    public function setTimeZone($request)
+    {
+        if($this->user && $this->user->getAttribute('timezone') !== null)
+        {
+            return Config::set('request.user.timezone', $this->user->getAttribute('timezone'));
+        }
+
+        return Config::set(
+            'request.user.timezone', 
+            $request->header(
+                'x-timezone', 
+                Config::get('app.user_timezone', Config::get('app.timezone'))
+            )
+        );
+
+    }
+}
