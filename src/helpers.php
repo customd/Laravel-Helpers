d1a6250d8214ee85a74113cd5997bed27964807e
Sam Sehnert <sam@customd.com>
2022-07-27 08:57:33 +1200

    chore(stan): resolve static analysis warnings

--- src/helpers.php
+++ src/helpers.php
@@ -14,30 +14,33 @@ if (! function_exists('execute')) {
         return app($classname)->execute(...$args);
     }
 }
-/*
- * dd() with headers
- */
+
 if (!function_exists('ddh')) {
+    /**
+     * dd() with headers
+     * @return void
+     */
     function ddh(...$vars){
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: *');
         header('Access-Control-Allow-Headers: *');
         $backfiles = debug_backtrace();
-        $vars[] = $backfiles[0]['file'] . ':' . $backfiles[0]['line'];
+        $vars[] = $backfiles[0]['file'] . ':' . $backfiles[0]['line']; // @phpstan-ignore-line
         dd(...$vars);
     }
 }
 
-/*
- * dump() with headers
- */
 if (!function_exists('dumph')) {
+    /**
+     * dumph() with headers
+     * @return void
+     */
     function dumph(...$vars){
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: *');
         header('Access-Control-Allow-Headers: *');
         $backfiles = debug_backtrace();
-        $vars[] = $backfiles[0]['file'] . ':' . $backfiles[0]['line'];
+        $vars[] = $backfiles[0]['file'] . ':' . $backfiles[0]['line']; // @phpstan-ignore-line
         dump(...$vars);
     }
 }
