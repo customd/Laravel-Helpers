5a1ad44bb21284a0052b063647eddc0e2c4f1036
Craig Smith <craig.smith@customd.com>
2023-02-23 10:58:52 +1300

    Update src/CdCarbonMixin.php

--- src/CdCarbonMixin.php
+++ src/CdCarbonMixin.php
@@ -95,6 +95,86 @@ class CdCarbonMixin
         };
     }
 
+    public function usersStartOfWeek(): Closure
+    {
+        $mixin = $this;
+        return static function (?int $day = null) use ($mixin) {
+            /** @var Carbon $date */
+            $date = self::this();
+            return $date->setTimezone($date->getUserTimezone())->startOfWeek($day)->setTimezone($mixin->systemTimezone);
+        };
+    }
+
+    public function usersEndOfWeek(): Closure
+    {
+        $mixin = $this;
+        return static function (?int $day = null) use ($mixin) {
+            /** @var Carbon $date */
+            $date = self::this();
+            return $date->setTimezone($date->getUserTimezone())->endOfWeek($day)->setTimezone($mixin->systemTimezone);
+        };
+    }
+
+    public function usersStartOfMonth(): Closure
+    {
+        $mixin = $this;
+        return static function () use ($mixin) {
+            /** @var Carbon $date */
+            $date = self::this();
+            return $date->setTimezone($date->getUserTimezone())->startOfMonth()->setTimezone($mixin->systemTimezone);
+        };
+    }
+
+    public function usersEndOfMonth(): Closure
+    {
+        $mixin = $this;
+        return static function () use ($mixin) {
+            /** @var Carbon $date */
+            $date = self::this();
+            return $date->setTimezone($date->getUserTimezone())->endOfMonth()->setTimezone($mixin->systemTimezone);
+        };
+    }
+
+    public function usersStartOfQuarter(): Closure
+    {
+        $mixin = $this;
+        return static function () use ($mixin) {
+            /** @var Carbon $date */
+            $date = self::this();
+            return $date->setTimezone($date->getUserTimezone())->startOfQuarter()->setTimezone($mixin->systemTimezone);
+        };
+    }
+
+    public function usersEndOfQuarter(): Closure
+    {
+        $mixin = $this;
+        return static function () use ($mixin) {
+            /** @var Carbon $date */
+            $date = self::this();
+            return $date->setTimezone($date->getUserTimezone())->endOfQuarter()->setTimezone($mixin->systemTimezone);
+        };
+    }
+
+    public function usersStartOfYear(): Closure
+    {
+        $mixin = $this;
+        return static function () use ($mixin) {
+            /** @var Carbon $date */
+            $date = self::this();
+            return $date->setTimezone($date->getUserTimezone())->startOfYear()->setTimezone($mixin->systemTimezone);
+        };
+    }
+
+    public function usersEndOfYear(): Closure
+    {
+        $mixin = $this;
+        return static function () use ($mixin) {
+            /** @var Carbon $date */
+            $date = self::this();
+            return $date->setTimezone($date->getUserTimezone())->endOfYear()->setTimezone($mixin->systemTimezone);
+        };
+    }
+
     public function parseWithTz(): Closure
     {
         return static function ($time) {
