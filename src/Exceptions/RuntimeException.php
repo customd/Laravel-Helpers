4fdd28c92e69811a0bfb80f0a67ab79046fa03da
Rick Hambrook <rick@customd.com>
2023-04-14 09:56:37 +1200

    feat(exception): update extra type to string|array|null

--- src/Exceptions/RuntimeException.php
+++ src/Exceptions/RuntimeException.php
@@ -9,10 +9,10 @@ class RuntimeException extends CoreException
 {
     public int $statusCode = 500;
 
-    public string $extra = '';
+    public string|array|null $extra;
 
 
-    public function __construct(string $message = "", string $extra = '', int $statusCode = 500, int $code = 0, Throwable $previous = null)
+    public function __construct(string $message = "", string|array|null $extra = null, int $statusCode = 500, int $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
         $this->extra = $extra;
@@ -20,14 +20,14 @@ class RuntimeException extends CoreException
     }
 
 
-    public static function err404(string $message = "Not Found", string $extra = ''): static
+    public static function err404(string $message = "Not Found", string|array|null $extra = null): static
     {
         //@phpstan-ignore-next-line
         return new static($message, $extra, 404);
     }
 
 
-    public static function err500(string $message = "Internal Server Error", string $extra = ''): static
+    public static function err500(string $message = "Internal Server Error", string|array|null $extra = null): static
     {
         //@phpstan-ignore-next-line
         return new static($message, $extra, 500);
