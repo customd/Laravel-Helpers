2af40d7016a603605184c9eb73a21f08d92c8b1e
Craig Smith <craigsmith@Craigs-MBP-2.localdomain>
2023-04-17 11:07:00 +1200

    chore: phpstan

--- src/Exceptions/RuntimeException.php
+++ src/Exceptions/RuntimeException.php
@@ -9,9 +9,22 @@ class RuntimeException extends CoreException
 {
     public int $statusCode = 500;
 
+    /**
+     *
+     * @var string|array<int|string, mixed>|null
+     */
     public string|array|null $extra;
 
 
+    /**
+     *
+     * @param string $message
+     * @param null|string|array<int|string, mixed> $extra
+     * @param int $statusCode
+     * @param int $code
+     * @param Throwable|null $previous
+     * @return void
+     */
     public function __construct(string $message = "", string|array|null $extra = null, int $statusCode = 500, int $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
@@ -19,14 +32,24 @@ class RuntimeException extends CoreException
         $this->statusCode = $statusCode;
     }
 
-
+    /**
+     *
+     * @param string $message
+     * @param null|string|array<int|string, mixed> $extra
+     * @return static
+     */
     public static function err404(string $message = "Not Found", string|array|null $extra = null): static
     {
         //@phpstan-ignore-next-line
         return new static($message, $extra, 404);
     }
 
-
+    /**
+     *
+     * @param string $message
+     * @param null|string|array<int|string, mixed> $extra
+     * @return static
+     */
     public static function err500(string $message = "Internal Server Error", string|array|null $extra = null): static
     {
         //@phpstan-ignore-next-line
