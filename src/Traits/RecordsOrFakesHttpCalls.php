701ec29f9cef0bf21ff374e9057a6e27bcf4f551
Craig Smith <craig.smith@customd.com>
2023-04-20 10:43:42 +1200

    Update file RecordsOrFakesHttpCalls.php

--- src/Traits/RecordsOrFakesHttpCalls.php
+++ src/Traits/RecordsOrFakesHttpCalls.php
@@ -7,15 +7,16 @@ use Illuminate\Support\Facades\Http;
 trait RecordsOrFakesHttpCalls
 {
 
-    protected $path = 'tests/stubs/';
+    protected string $path = 'tests/stubs/';
 
-    protected $record = false;
+    protected bool $record = false;
 
 
     protected function storeHttpRecordings(string $name, $type = 'html'): void
     {
         $recordings = Http::recorded();
         $path = base_path($this->path);
+
         foreach ($recordings as $i => $recording) {
             /** @var \Illuminate\Http\Client\Response $response */
             $response = $recording[1];
@@ -38,7 +39,53 @@ trait RecordsOrFakesHttpCalls
         $callback();
 
         if ($this->record === true) {
-            $this->storeRecordings($name, $type);
+            $this->storeHttpRecordings($name, $type);
         }
+    }
+
+    protected function getRecordings($idx = null, $raw = false)
+    {
+        $items =  Http::recorded()->map(function ($recording) use ($raw) {
+            /** @var \Illuminate\Http\Client\Request $request*/
+            $request = $recording[0];
+            /** @var \Illuminate\Http\Client\Response $response */
+            $response = $recording[1];
+            if ($raw === true) {
+                return [
+                    'request'  => $request,
+                    'response' => $response
+                ];
+            }
+            return [
+                'request'  => [
+                    'headers' => $request->headers(),
+                    'body'    => $request->body(),
+                    'method'  => $request->method(),
+                    'url'     => $request->url(),
+                ],
+                'response' => [
+                    'body'    => $response->body(),
+                    'status'  => $response->status(),
+                    'headers' => $response->headers(),
+                ],
+            ];
+        });
+
+        return $idx ? $items->get(--$idx) : $items;
+    }
+
+    protected function getRecordedRequests($idx = null)
+    {
+
+        $recordings = $this->getRecordings()->map(fn ($recording) => $recording['request']);
+
+        return $idx ? $recordings->get(--$idx) : $recordings;
+    }
+
+    protected function getRecordedResponses($idx = null)
+    {
+        $recordings = $this->getRecordings()->map(fn ($recording) => $recording['response']);
+
+        return $idx ? $recordings->get(--$idx) : $recordings;
     }
 }
